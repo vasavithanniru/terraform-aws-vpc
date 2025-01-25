b1facd7fa014ce38643754c9df87d8ff4be23e8d
vasavi <tvasu370@gmail.com>
2025-01-25 12:28:03 +0530

    infra devolopment

--- main.tf
+++ main.tf
@@ -166,13 +166,13 @@ resource "aws_route" "public" {
 resource "aws_route" "private_nat"{
   route_table_id = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.main.id
+  nat_gateway_id = aws_nat_gateway.main.id
 }
 
 resource "aws_route" "database_nat" {
   route_table_id = aws_route_table.database.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.main.id
+  nat_gateway_id = aws_nat_gateway.main.id
 }
 
 # associating public-route-table to public subnets(1a, 1b)
