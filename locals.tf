6845e05ad46ca6df025885d4564e67a2bd301ced
vasavi <tvasu370@gmail.com>
2025-01-26 18:49:31 +0530

    vpc development

--- locals.tf
+++ locals.tf
@@ -3,7 +3,3 @@ locals {
     az_names = slice(data.aws_availability_zones.available.names, 0, 2)
 }
 
-locals {
-  resource_name = "${var.project_name}-${var.environment}"
-  az_names = slice(data.aws_availability_zones.available.names, 0, 2)
-}
